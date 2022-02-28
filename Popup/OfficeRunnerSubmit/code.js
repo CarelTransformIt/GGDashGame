194cc7b1f780feea7c36e387469f275168ee0d48
CarelTransformIt <carel.TransformIt@gmail.com>
2022-02-28 16:33:48 +0200

    Lets hope

--- Popup/OfficeRunnerSubmit/code.js
+++ Popup/OfficeRunnerSubmit/code.js
@@ -20,7 +20,7 @@ function submitClick(){
 
   var objectToSubmit = Object.assign(userInfoObject,payloadObject);
 
-  var postURL = window.location.href.includes('localhost') ? 'https://localhost:44347/GGDash/postSubmission' : 'https://localhost:44347/GGDash/postSubmission'
+  var postURL = window.location.href.includes('localhost') ? 'https://localhost:44347/GGDash/postSubmission' : 'https://gamesgalore.azurewebsites.net/GGDash/postSubmission'
 
   $.post(postURL,
     objectToSubmit,
@@ -58,7 +58,8 @@ function setScore(){
 
 function shareToFacebook(){
   var sharetoken = sessionStorage.getItem('shareToken');
-  window.open(`https://www.facebook.com/sharer.php?u=gamesgalorefrontend.azurewebsites.net/GGDash/Share?token=${sharetoken}`);
+  window.open(`https://www.facebook.com/sharer.php?u=https%3A%2F%2Fgamesgalorefrontend.azurewebsites.net%2FGGDash%2Fshare%3Ftoken%3D${sharetoken}`);
+  //window.open(`https://www.facebook.com/sharer.php?u=gamesgalorefrontend.azurewebsites.net/GGDash/Share?token=HqceX4jppeTclh%2fKo9PMXIKjF3JDTB5FW5wV5dJJA29%2fDCW%2bgX5j6arhn8G14WvQGjWi5tZ5dBYWrio4utSMyaWGQViMzKuPrDb1MPamgbHdRRRA3OssAg0oa82wjyc2k4UqCPlSkywTqT3VjW0vmIwFaCIZFKrRGqsqWwRr0P3DFsscu4YwSpRSEIo5bv%2bVwVF9gQRanQ4eE4u0DWMmsE0fm%2fnI4ZIwBNPcQVPzhHk%3d`);
 }
 
 function returnToGame(){
